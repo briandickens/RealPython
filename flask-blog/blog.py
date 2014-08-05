605834189b4507d1f5e8f19b1c2ad5aea85640b8
briandickens <briandickens@gmail.com>
2014-08-05 18:04:41 -0400

    Get login working.
    Add code to allow for login and logout.

--- flask-blog/blog.py
+++ flask-blog/blog.py
@@ -20,7 +20,7 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
@@ -29,11 +29,17 @@ def login():
         else:
             session['logged in'] = True
             return redirect(url_for('main'))
-    return render_template('login.html', error = error)
+    return render_template('login.html', error=error)
 
 @app.route('/main')
 def main():
     return render_template('main.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out.')
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
