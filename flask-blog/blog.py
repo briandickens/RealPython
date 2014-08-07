ccb05b5b68a0a289a4910057d0155b0c0975f611
briandickens <briandickens@gmail.com>
2014-08-07 17:21:57 -0400

    Actually got login working.
    For real this time,

--- flask-blog/blog.py
+++ flask-blog/blog.py
@@ -4,6 +4,7 @@
 from flask import Flask, render_template, request, session, flash, \
         redirect, url_for, g
 import sqlite3
+from functools import wraps
 
 # configuration
 DATABASE = 'blog.db'
@@ -20,6 +21,16 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
 @app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
@@ -27,11 +38,13 @@ def login():
         if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
             error = 'Invalid Credentials. Please try again.'
         else:
-            session['logged in'] = True
+            session['logged_in'] = True
             return redirect(url_for('main'))
     return render_template('login.html', error=error)
 
+
 @app.route('/main')
+@login_required
 def main():
     return render_template('main.html')
 
