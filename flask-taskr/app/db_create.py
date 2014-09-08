d199e7f671cd16c1756561f1e4d4b8d7c2e1d0f0
Brian Dickens <briandickens@gmail.com>
2014-09-08 16:36:57 -0400

    finished implementing sqlalchemy

--- flask-taskr/app/db_create.py
+++ flask-taskr/app/db_create.py
@@ -5,25 +5,16 @@ Create the db for the app.
 
 @author: bdickens
 '''
+from views import db
+from models import FTasks
+from datetime import date
 
-import sqlite3
-from config import DATABASE_PATH
+# create the database and the db Table
+db.create_all()
 
-with sqlite3.connect(DATABASE_PATH) as connection:
-    
-    # get a cursor object used to exectue SQL Commands
-    c = connection.cursor()
-    
-    # create the Table
+# insert data
+db.session.add(FTasks("Finish this tutorial", date(2014, 3, 13), 10, 1))
+db.session.add(FTasks("Finish Real Python", date(2014, 3, 13), 10, 1))
 
-#     c.execute("""CREATE TABLE ftasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-#             name TEXT NOT NULL,
-#             due_date TEXT NOT NULL,
-#             priority INTEGER NOT NULL,
-#             status INTEGER NOT NULL)""")
- 
-    # insert dummy data into the table
-    c.execute('INSERT INTO ftasks(name, due_date, priority, status) VALUES("Finish this tutorial", "02/03/2014", 10, 1)')
-
-#    c.execute('INSERT INTO ftasks(name, due_date, priority, status) VALUES("Finish Real Python Course 2", "02/03/2014", 10, 1)')
-    
+# commit the Changes
+db.session.commit()
